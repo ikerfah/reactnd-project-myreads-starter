87689a9eb3dc7f2a2f01fe7ae7f2160c9542f5b8
KERFAH Issam <i.kerfah@gmail.com>
2021-08-09 02:10:11 +0300

    feat: Show empty shelf message if empty

--- src/BooksShelf.js
+++ src/BooksShelf.js
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 class BooksShelf extends Component {
-    onShelfChanged = (book,newShelfValue) =>{
-        if(this.props.onShelfChanged){
-            this.props.onShelfChanged(book,newShelfValue)
+    onShelfChanged = (book, newShelfValue) => {
+        if (this.props.onShelfChanged) {
+            this.props.onShelfChanged(book, newShelfValue)
         }
     }
     render() {
-        const { title,books} = this.props
-        
-        const booksComponents = books.map((book) =>(
-            <Book book={book} onShelfChanged={this.onShelfChanged}/>
+        const { title, books } = this.props
+
+        const booksComponents = books.map((book) => (
+            <Book book={book} onShelfChanged={this.onShelfChanged} />
         ))
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {booksComponents}
-                    </ol>
+                    {(!books || books.length == 0) ?
+                        <span>Shelf is empty</span> :
+                        <ol className="books-grid">
+                            {booksComponents}
+                        </ol>}
                 </div>
             </div>
         )
