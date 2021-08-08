1f5f1923aebc5f0fb7b6fbaedf66577e3ec00be7
KERFAH Issam <i.kerfah@gmail.com>
2021-08-09 01:06:46 +0300

    feat: fetch all books from API

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BooksShelf from './BooksShelf'
-
+import * as BooksAPI from './BooksAPI'
 class BooksApp extends React.Component {
   state = {
+    books: [],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -14,7 +15,16 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => this.setState({ books }))
+  }
+
   render() {
+
+    const currentlyReading = this.state.books.filter((book) => book.shelf === 'currentlyReading')
+    const wantToRead = this.state.books.filter((book) => book.shelf === 'wantToRead')
+    const read = this.state.books.filter((book) => book.shelf === 'read')
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -22,14 +32,6 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input type="text" placeholder="Search by title or author" />
 
               </div>
@@ -45,9 +47,9 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <BooksShelf title={"Currently Reading"} />
-                <BooksShelf title={"Want to Read"} />
-                <BooksShelf title={"Read"} />
+                <BooksShelf title={"Currently Reading"} books={currentlyReading} />
+                <BooksShelf title={"Want to Read"} books={wantToRead} />
+                <BooksShelf title={"Read"} books={read} />
               </div>
             </div>
             <div className="open-search">
