cfa87343170c300f1807b5f3c051f60e7ef62eb7
KERFAH Issam <i.kerfah@gmail.com>
2021-08-09 22:33:51 +0300

    refactor: component class to function class where needed

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import BookAuthors from './BookAuthors';
 import BookShelfChanger from './BookShelfChanger';
-class Book extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+const Book = (props) => {
 
-    handleOnShelfChanged = (newShelfValue) => {
-        if (this.props.onShelfChanged) {
-            this.props.onShelfChanged(this.props.book, newShelfValue)
+    const handleOnShelfChanged = (newShelfValue) => {
+        if (props.onShelfChanged) {
+            props.onShelfChanged(props.book, newShelfValue)
         }
     }
-    render() {
-        const { book } = this.props
-        return (
-            <li key={book.id}>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: (book.imageLinks && book.imageLinks.thumbnail) ? `url("${book.imageLinks.thumbnail}")` : 'none' }}></div>
-                        <BookShelfChanger onShelfChanged={this.handleOnShelfChanged} defaultShelf={book.shelf} />
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    {book.authors && <BookAuthors authors={book.authors} />}
+    const { book } = props
+    return (
+        <li key={book.id}>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: (book.imageLinks && book.imageLinks.thumbnail) ? `url("${book.imageLinks.thumbnail}")` : 'none' }}></div>
+                    <BookShelfChanger onShelfChanged={handleOnShelfChanged} defaultShelf={book.shelf} />
                 </div>
-            </li>
-        )
-    }
+                <div className="book-title">{book.title}</div>
+                {book.authors && <BookAuthors authors={book.authors} />}
+            </div>
+        </li>
+    )
 }
 
 Book.propTypes = {
--- src/BooksShelf.js
+++ src/BooksShelf.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
-class BooksShelf extends Component {
-    onShelfChanged = (book, newShelfValue) => {
-        if (this.props.onShelfChanged) {
-            this.props.onShelfChanged(book, newShelfValue)
+const BooksShelf = (props) => {
+    const onShelfChanged = (book, newShelfValue) => {
+        if (props.onShelfChanged) {
+            props.onShelfChanged(book, newShelfValue)
         }
     }
-    render() {
-        const { title, books } = this.props
+    const { title, books } = props
 
-        const booksComponents = books.map((book) => (
-            <Book key={book.id} book={book} onShelfChanged={this.onShelfChanged} />
-        ))
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{title}</h2>
-                <div className="bookshelf-books">
-                    {(!books || books.length === 0) ?
-                        <span>Shelf is empty</span> :
-                        <ol className="books-grid">
-                            {booksComponents}
-                        </ol>}
-                </div>
+    const booksComponents = books.map((book) => (
+        <Book key={book.id} book={book} onShelfChanged={onShelfChanged} />
+    ))
+
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                {(!books || books.length === 0) ?
+                    <span>Shelf is empty</span> :
+                    <ol className="books-grid">
+                        {booksComponents}
+                    </ol>}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 BooksShelf.propTypes = {
